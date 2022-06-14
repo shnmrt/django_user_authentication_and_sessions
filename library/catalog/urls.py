38a77aa2591f288f44443302896b60d04d0ddf93
Murat Şahin <sahinmurat3@gmail.com>
2022-06-14 21:01:53 +0300

    user authentication protocols pn views

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns =[
     path('', views.index, name='index'),
     path('create_book/', views.BookCreate.as_view(), name='create_book'),
-    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail')
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+    path('my_view', views.my_view, name='my_view')
 ]
--- library/catalog/views.py
+++ library/catalog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre, Language
 from django.views.generic import CreateView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def index(request):
@@ -19,10 +21,13 @@ def index(request):
     }
     return render(request, 'catalog/index.html',context=context)
 
-class BookCreate(CreateView): # model_form.html
+class BookCreate(LoginRequiredMixin,CreateView): # model_form.html
     model = Book
     fields = '__all__'
 
 class BookDetail(DetailView):
     model = Book
-    
+
+@login_required
+def my_view(request):
+    return render(request,'catalog/my_view.html')
